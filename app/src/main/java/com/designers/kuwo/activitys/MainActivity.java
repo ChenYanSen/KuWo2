824121b5871d6ca153d5ecb03e7919a3ea1d2e54
ChenYanSen <1140169017@qq.com>
2017-01-02 18:59:16 +0800

    Initial commit

---
+++ app/src/main/java/com/designers/kuwo/activitys/MainActivity.java
@@ -0,0 +1,47 @@
+package com.designers.kuwo.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.designers.kuwo.R;
+import com.designers.kuwo.utils.T;
+
+
+public class MainActivity extends Activity {
+    private TextView demo_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.demo_tv = (TextView) findViewById(R.id.demo_tv);
+        this.demo_tv.setOnClickListener(x -> {
+            T.showLong(this, "2017你好！");
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

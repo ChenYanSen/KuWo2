1845e7cc99691caf3b53425db69f415844ac1956
PC-CWB <1159076075@qq.com>
2017-01-03 20:07:41 +0800

    测试提交 41

---
+++ app/src/main/java/com/designers/kuwo/activitys/SingleFragment.java
@@ -0,0 +1,28 @@
+package com.designers.kuwo.activitys;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.designers.kuwo.R;
+
+public class SingleFragment extends Fragment {
+
+    private Button btnSingle;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_single, container, false);
+        return view;
+    }
+
+    //**********************//
+}

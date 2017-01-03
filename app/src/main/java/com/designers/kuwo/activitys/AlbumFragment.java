9cd869624c94a41d759d4dbb0be3d441a1f4c485
PC-CWB <1159076075@qq.com>
2017-01-03 19:49:11 +0800

    测试提交 2

--- app/src/main/java/com/designers/kuwo/activitys/AlbumFragment.java
+++ app/src/main/java/com/designers/kuwo/activitys/AlbumFragment.java
@@ -14,6 +14,7 @@ public class AlbumFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    //**********************//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_album,container,false);

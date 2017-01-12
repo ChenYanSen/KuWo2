974b76ada22e934baec1087bc67070f9e572bc78
ChenYanSen <1140169017@qq.com>
2017-01-12 09:45:19 +0800

    cys

--- app/src/main/java/com/designers/kuwo/sqlite/SqLiteDatabaseHelper.java
+++ app/src/main/java/com/designers/kuwo/sqlite/SqLiteDatabaseHelper.java
@@ -31,32 +31,32 @@ public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
 		String sql_cascade = "PRAGMA foreign_keys=ON";
 		sqLiteDatabase.execSQL(sql_cascade);
 		/**
-		 * 创建user表，用户的信息
+		 * 创建user表，用户的信息 ID 账号，密码  爱好  昵称  个性签名 性别 地区 头像
 		 */
-		String sql_user = "create table user(userId integer primary key autoincrement,account varchar(20),Password varchar(20),hobby varchar(30),nickname varchar(20),signature varchar(50),sex varchar(10),area varchar(50),avatar varchar(100))";
+		String sql_user = "create table user(userId integer primary key autoincrement,account varchar(20),Password varchar(20),hobby varchar(30),nickname varchar(20),signature txt,sex varchar(10),area varchar(50),avatar varchar(100))";
 		sqLiteDatabase.execSQL(sql_user);
 		/**
-		 * 创建音乐表，
+		 * 创建音乐表，ID 歌名 歌手 存储路径 图片 歌词 详细信息
 		 */
 		String sql_songs = "create table songs(songId integer primary key autoincrement,songName varchar(20),singer varchar(20),songUri varchar(30),songImage varchar(100)，singlyrics text，information varchar(50))";
 		sqLiteDatabase.execSQL(sql_songs);
 		/**
-		 * 创建collection表
+		 * 创建collection表  ID 歌名（参照音乐的歌名） 账号（参照用户的账号）
 		 */
 		String sql_collection= "create table collection(collectionId integer primary key autoincrement, collectionSongName varchar(20) references songs(songName),collectionAccount varchar(20) references user(account))";
 		sqLiteDatabase.execSQL(sql_collection);
 		/**
-		 * 创建歌单表，
+		 * 创建歌单表，ID 歌单名  歌名（参照音乐的歌名） 账号（参照用户的账号）
 		 */
 		String sql_songList= "create table songList(songListId integer primary key autoincrement,songListName varchar(20) ,songListSongName varchar(20) references songs(songName),songListAccount varchar(20) references user(account))";
 		sqLiteDatabase.execSQL(sql_songList);
 		/**
-		 * 创建专辑
+		 * 创建专辑  ID  专辑名 歌名（参照音乐的歌名）  账号（参照用户的的账号）
 		 */
 		String sql_album = "create table album(albumId integer primary key autoincrement,albumName varchar(20),albumSongName varchar(20) references songs(songName),albumAccount varchar(20) references user(account))";
 		sqLiteDatabase.execSQL(sql_album);
 		/**
-		 * 创建播放列表
+		 * 创建播放列表  ID 歌名（参照音乐的歌名）
 		 */
 		String sql_playList = "create table playList(playList integer primary key autoincrement,palyListSongName varchar(20) references songs(songName))";
 		sqLiteDatabase.execSQL(sql_playList);

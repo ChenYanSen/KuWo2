a0450be94fbfd522ba9aa3e8c99026e036602a74
PC-CWB <1159076075@qq.com>
2017-03-06 09:38:25 +0800

    2017/3/6本地音乐与主页结合提交 111

---
+++ app/src/main/java/com/designers/kuwo/biz/bizimpl/SongListBizImpl.java
@@ -0,0 +1,133 @@
+package com.designers.kuwo.biz.bizimpl;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.designers.kuwo.biz.SongListBiz;
+import com.designers.kuwo.dao.SongListDao;
+import com.designers.kuwo.dao.daoimpl.SongListDaoImpl;
+import com.designers.kuwo.eneity.SongList;
+import com.designers.kuwo.sqlite.SQLiteDatabaseManager;
+import com.designers.kuwo.sqlite.TransactionManager;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/2/21.
+ */
+public class SongListBizImpl implements SongListBiz {
+
+    private SongListDao songListDao;
+
+    public SongListBizImpl() {
+        this.songListDao = new SongListDaoImpl();
+    }
+
+    @Override
+    public void insert(Context context, SongList songList) {
+        SQLiteDatabaseManager sqLiteDatabaseManager = new SQLiteDatabaseManager();
+        SQLiteDatabase sqLiteDatabase = sqLiteDatabaseManager.getDatabaseByWrite(context);
+        //开启事务
+        TransactionManager transactionManager = new TransactionManager();
+        transactionManager.beginTransaction(sqLiteDatabase);
+
+        try {
+            this.songListDao.insert(sqLiteDatabase, songList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            transactionManager.commitTransaction(sqLiteDatabase);
+            transactionManager.endTransaction(sqLiteDatabase);
+            sqLiteDatabaseManager.closeSQLiteDatabase(sqLiteDatabase);
+        }
+    }
+
+    @Override
+    public List<Map<String, Object>> findNameAndSize(Context context, final String userName) {
+        SQLiteDatabaseManager sqLiteDatabaseManager = new SQLiteDatabaseManager();
+        SQLiteDatabase sqLiteDatabase = sqLiteDatabaseManager.getDatabaseByRead(context);
+
+        try {
+            return this.songListDao.selectNameAndSize(sqLiteDatabase, userName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            sqLiteDatabaseManager.closeSQLiteDatabase(sqLiteDatabase);
+        }
+    }
+
+    @Override
+    public void update(Context context, String oldName, String newName, String userName) {
+        SQLiteDatabaseManager sqLiteDatabaseManager = new SQLiteDatabaseManager();
+        SQLiteDatabase sqLiteDatabase = sqLiteDatabaseManager.getDatabaseByWrite(context);
+        //开启事务
+        TransactionManager transactionManager = new TransactionManager();
+        transactionManager.beginTransaction(sqLiteDatabase);
+
+        try {
+            this.songListDao.update(sqLiteDatabase, oldName, newName, userName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            transactionManager.commitTransaction(sqLiteDatabase);
+            transactionManager.endTransaction(sqLiteDatabase);
+            sqLiteDatabaseManager.closeSQLiteDatabase(sqLiteDatabase);
+        }
+    }
+
+    @Override
+    public void delete(Context context, String songListName, String userName) {
+        SQLiteDatabaseManager sqLiteDatabaseManager = new SQLiteDatabaseManager();
+        SQLiteDatabase sqLiteDatabase = sqLiteDatabaseManager.getDatabaseByWrite(context);
+        //开启事务
+        TransactionManager transactionManager = new TransactionManager();
+        transactionManager.beginTransaction(sqLiteDatabase);
+
+        try {
+            this.songListDao.delete(sqLiteDatabase, songListName, userName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            transactionManager.commitTransaction(sqLiteDatabase);
+            transactionManager.endTransaction(sqLiteDatabase);
+            sqLiteDatabaseManager.closeSQLiteDatabase(sqLiteDatabase);
+        }
+    }
+
+    @Override
+    public List<Map<String, Object>> findAll(Context context) {
+        SQLiteDatabaseManager sqLiteDatabaseManager = new SQLiteDatabaseManager();
+        SQLiteDatabase sqLiteDatabase = sqLiteDatabaseManager.getDatabaseByRead(context);
+
+        try {
+            return this.songListDao.select(sqLiteDatabase);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            sqLiteDatabaseManager.closeSQLiteDatabase(sqLiteDatabase);
+        }
+    }
+
+    @Override
+    public void deleteSong(Context context, String songListName, String songName, String userName) {
+        SQLiteDatabaseManager sqLiteDatabaseManager = new SQLiteDatabaseManager();
+        SQLiteDatabase sqLiteDatabase = sqLiteDatabaseManager.getDatabaseByWrite(context);
+        //开启事务
+        TransactionManager transactionManager = new TransactionManager();
+        transactionManager.beginTransaction(sqLiteDatabase);
+
+        try {
+            this.songListDao.deleteSong(sqLiteDatabase, songListName, songName, userName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            transactionManager.commitTransaction(sqLiteDatabase);
+            transactionManager.endTransaction(sqLiteDatabase);
+            sqLiteDatabaseManager.closeSQLiteDatabase(sqLiteDatabase);
+        }
+    }
+}
